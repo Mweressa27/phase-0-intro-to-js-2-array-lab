3dc6ada436c79ff03d5377a84abf112daff8dcd7
Mweresa Esther Magoma <esthermweresa27@gmail.com>
2025-03-13 05:32:06 +0300

    complete tests

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const newArray = [...cats, 'Broom',]
+    return newArray
+}
+
+function  prependCat(name) {
+    const anotherArray = ['Arnold', ...cats]
+    return anotherArray
+}
+
+function removeLastCat() {
+    const arrayOne = cats.slice(0, -1)
+    return arrayOne
+}
+
+function removeFirstCat() {
+    const arrayTwo = cats.slice(1)
+    return arrayTwo
+}
+
+
+
